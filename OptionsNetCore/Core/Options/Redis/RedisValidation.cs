3ebe80a07a2f61f8771d9033c1898b6249f71fc8
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-10-24 18:38:42 -0500

    Agregar archivos de proyecto.

---
+++ OptionsNetCore/Core/Options/Redis/RedisValidation.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace OptionsNetCore.Core.Options.Redis
+{
+    public class RedisValidation : IValidateOptions<RedisOptions>
+    {
+        public ValidateOptionsResult Validate(string name, RedisOptions options)
+        {
+            string failures = null;
+
+            if (options.ConnectRetry > 3 && options.ConnectTimeout <= 5000)
+                failures = "ConnectTimeout must be > than 5000";
+
+            if (!string.IsNullOrEmpty(failures))
+                return ValidateOptionsResult.Fail(failures);
+
+            return ValidateOptionsResult.Success;
+        }
+    }
+}
